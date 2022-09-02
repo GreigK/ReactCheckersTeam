773b83e87faaebb81bbbdc530b50124be3bcb4d4
D-oro <107217252+D-oro@users.noreply.github.com>
2022-09-02 16:08:50 +0100

    src folder setup

--- src/components/x1y6.js
+++
@@ -1,9 +0,0 @@
-import {redBlackFree} from '../controllers/RedBlackFree';
-import {useState} from 'react';
-
-const x1y6 = () => {
-    const [piece, setPiece] = useState({
-        variable: "red"
-    })
-}
-
--- src/components/x2y7.js
+++
@@ -1,7 +0,0 @@
-// can checker move forward?
-// move checker
-// can checker take another checker
-//     is another checker of the other color present?
-
-
-
