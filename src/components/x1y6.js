1af807839d9c69367098df705bbd06af5e78f597
D-oro <107217252+D-oro@users.noreply.github.com>
2022-09-02 11:33:45 +0100

    pseudocoded potential moves and created functions for piece states

---
+++ src/components/x1y6.js
@@ -0,0 +1,9 @@
+import {redBlackFree} from '../controllers/RedBlackFree';
+import {useState} from 'react';
+
+const x1y6 = () => {
+    const [piece, setPiece] = useState({
+        variable: "red"
+    })
+}
+
