a797a75b1f07a234b0ee79738a9e2009f3e83353
D-oro <107217252+D-oro@users.noreply.github.com>
2022-09-07 14:29:58 +0100

    we now also have flappy birds and chess for no reason

--- src/App.js
+++ src/App.js
@@ -1,134 +1,15 @@
+import {Chessboard, chessboard} from 'react-chessboard'
 import './App.css';
 import GameBoard from './container/GameBoard'
+import FlappyBirds from './container/FlappyBirds';
 
 function App() {
   return (
     <div className="App">
+      router
       <GameBoard/>
-
-      {/* <header id="app-header">
-      
-        <section className="header-section hsl">
-          very normal checkers
-        </section>
-
-        <section className="header-section hsm">
-          and landmines
-        </section>
-  
-        <section className="header-section hsr">
-          and more...
-        </section>
-
-      </header>
-
-
-      <main id="main-content"> */}
-
-        {/* <aside className="player-box pb1">1</aside> */}
-
-        {/* <section id="chessboard-container">
-
-        <tr>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-        </tr>
-
-        <tr>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-        </tr>
-
-        <tr>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-        </tr>
-
-        <tr>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-        </tr>
-
-        <tr>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-        </tr>
-
-        <tr>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-        </tr>
-
-        <tr>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-        </tr>
-
-        <tr>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-                <td className='white-sq'>2</td>
-                <td className='black-sq'>3</td>
-                <td className='white-sq'>4</td>
-                <td className='black-sq'>1</td>
-        </tr>
-
-        </section> */}
-        
-        {/* <aside className="player-box pb2">2</aside> */}
-
-
-
-
-
-
-      {/* </main> */}
-
-
+      <Chessboard id="BasicBoard"/>
+      <FlappyBirds/>
     </div>
   );
 }
---
+++ src/container/FlappyBirds.js
@@ -0,0 +1,136 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const Bird = styled.div`
+position: absolute;
+background-color: red;
+height: ${(props) => props.size}px;
+width: ${(props) => props.size}px;
+top: ${(props) => props.top}px;
+border-radius: 50%;`
+
+const PageStyle = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+& span{
+  color: white;
+  font-size: 24px;
+  position: absolute;
+}`
+
+const GameBox = styled.div`
+height: ${(props) => props.height}px;
+width: ${(props) => props.width}px;
+background-color: black;
+overflow: hidden;`
+
+const Obstacle = styled.div`
+position: relative;
+top: ${(props) => props.top}px;
+height: ${(props) => props.height}px;
+width: ${(props) => props.width}px;
+left: ${(props) => props.left}px;
+background-color: green;`
+
+const bird_size = 20;
+const game_width = 500;
+const game_height = 500;
+const gravity = 4;
+const heightOfJump = 100;
+const obstacle_width = 40;
+const obstacle_gap = 200;
+
+function FlappyBirds() {
+
+  const [positionOfBird, setPositionOfBird] = useState(250);
+  const [gameHasStarted, setGameHasStarted] = useState(false);
+  const [obstacleHeight, setObstacleHeight] = useState(100);
+  const [obstacleLeft, setObstacleLeft] = useState(game_width - obstacle_width);
+  const [score, setScore] = useState(0);
+
+  const bottomObstacleHeight = game_height - obstacle_gap - obstacleHeight;
+
+  useEffect(() => {
+    let timeId;
+    if (gameHasStarted && positionOfBird < game_height - bird_size) {
+      timeId = setInterval(() => {
+        setPositionOfBird(positionOfBird => positionOfBird + gravity);
+      }, 24);
+    }
+    return () => {
+      clearInterval(timeId);
+    }
+  }, [positionOfBird, gameHasStarted, score]);
+
+  useEffect(() => {
+    let obstacleId;
+    if (gameHasStarted && obstacleLeft >= -obstacle_width) {
+      obstacleId = setInterval(() => {
+        setObstacleLeft((obstacleLeft) => obstacleLeft - 3);
+      }, 24);
+
+      return () => {
+        clearInterval(obstacleId)
+      };
+    }
+    else {
+      setObstacleLeft(game_width - obstacle_width);
+      setObstacleHeight(Math.floor(Math.random() * (game_height - obstacle_gap)))
+      setScore(score => (score + 1))
+    }
+  }, [gameHasStarted, obstacleLeft]);
+
+
+
+  useEffect(() => {
+    const hasCollidedWithTopObstacle = positionOfBird >= 0 && positionOfBird < obstacleHeight;
+    const hasCollidedWithBottomObstacle = positionOfBird <= 500 && positionOfBird >= 500 - bottomObstacleHeight;
+
+    if (
+      obstacleLeft >= 0 &&
+      obstacleLeft <= obstacle_width &&
+      (hasCollidedWithTopObstacle || hasCollidedWithBottomObstacle)
+    ) {
+      setGameHasStarted(false)
+    }
+  }, [positionOfBird, obstacleHeight, bottomObstacleHeight, obstacleLeft]);
+
+
+  const handleClick = () => {
+    const newPositionOfBird = positionOfBird - heightOfJump;
+    if (!gameHasStarted) {
+      setGameHasStarted(true);
+    } else if (newPositionOfBird < 0) {
+      setPositionOfBird(0)
+    } else {
+      setPositionOfBird(newPositionOfBird);
+    }
+  };
+
+  return (
+    <div>
+    <PageStyle onClick={handleClick}>
+      <GameBox height={game_height} width={game_width}>
+        <Obstacle
+          top={0}
+          width={obstacle_width}
+          height={obstacleHeight}
+          left={obstacleLeft}
+        />
+        <Bird size={bird_size} top={positionOfBird} />
+        <Obstacle
+          top={game_height - (obstacleHeight + bottomObstacleHeight)}
+          width={obstacle_width}
+          height={bottomObstacleHeight}
+          left={obstacleLeft}
+        />
+      </GameBox>
+      <span>{score}</span>
+    </PageStyle>
+    <img src='/chcik.jpg'></img>
+    </div>
+  );
+}
+
+export default FlappyBirds;
